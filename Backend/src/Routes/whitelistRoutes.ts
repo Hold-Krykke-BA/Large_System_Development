87e3355d78721837b57a7ef4d46e699d3a24067c
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-11-24 17:03:12 +0100

    ipRoutes

--- Backend/src/Routes/whitelistRoutes.ts
+++ Backend/src/Routes/whitelistRoutes.ts
@@ -2,10 +2,30 @@ import express from "express";
 const router = express.Router();
 import WhitelistService from "../Services/whitelistService"
 
-router.post('/', async function (req, res, next) {
+
+router.get('/', async function (req: any, res, next) {
   try {
-    let newCourse = req.body;
-    const status = await WhitelistService.getWhitelist();
+    const whitelist = await WhitelistService.getWhitelist();
+    res.json(whitelist);
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.put('/addip', async function (req, res, next) {
+  try {
+    let newIP = req.body.ip;
+    const status = await WhitelistService.addToWhitelist(newIP);
+    res.json({ status })
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.delete('/removeip/:ip', async function (req, res, next) {
+  try {
+    let ip = req.params.ip
+    const status = await WhitelistService.removeFromWhitelist(ip);
     res.json({ status })
   } catch (err) {
     next(err);
