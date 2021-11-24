80882702e81e74fd11cffe3d489aa86aa88fa4b8
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-11-24 16:40:06 +0100

    courseRoutes

--- Backend/src/Routes/courseRoutes.ts
+++ Backend/src/Routes/courseRoutes.ts
@@ -1,17 +1,8 @@
 import express from "express";
 const router = express.Router();
-import connection from "../Services/databaseService"
-import UserService from "../Services/userService"
 import CourseService from "../Services/courseService"
-import AttendanceCheckService from "../Services/attendanceCheckService"
 
-
-// (async function setup() {
-//   const client = await connection();
-//   await CourseService.setDatabase(client)
-// })()
-
-router.post('/', async function (req, res, next) {
+router.post('/add', async function (req, res, next) {
   try {
     let newCourse = req.body;
     const status = await CourseService.addCourse(newCourse)
@@ -20,5 +11,34 @@ router.post('/', async function (req, res, next) {
     next(err);
   }
 })
+
+router.get('/all', async function (req: any, res, next) {
+  try {
+    const courses = await CourseService.getAllCourses();
+    res.json(courses);
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.get('/:courseID', async function (req: any, res, next) {
+  try {
+    const courseID = req.params.courseID;
+    const course = await CourseService.getCourse(courseID);
+    res.json(course);
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.delete('/remove/:courseID', async function (req: any, res, next) {
+  try {
+    const courseID = req.params.courseID;
+    const course = await CourseService.deleteCourse(courseID);
+    res.json(course);
+  } catch (err) {
+    next(err)
+  }
+});
 
 module.exports = router;
