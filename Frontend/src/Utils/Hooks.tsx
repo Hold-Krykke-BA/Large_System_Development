7fef78c821dc9e4756d9bb8b56ccde5396dc9a5a
Runi <Runiniclasen@hotmail.com>
2021-12-01 21:48:30 +0100

    commit b4 merge

---
+++ Frontend/src/Utils/Hooks.tsx
@@ -0,0 +1,11 @@
+//get URL from .env
+//see fetchAPI & hooks from wrk
+
+export const useBackend = () => {
+
+    const getApiOne = () => {};
+    const getApiTwo = () => {};
+
+    return {getApiOne, getApiTwo} //right way? no
+};
+
