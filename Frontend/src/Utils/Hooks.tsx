beec9948ce7c1dc71c1ebb86f2053fcd427ddce3
Runi <Runiniclasen@hotmail.com>
2021-12-02 20:20:04 +0100

    add course backend

--- Frontend/src/Utils/Hooks.tsx
+++ Frontend/src/Utils/Hooks.tsx
@@ -1,5 +1,6 @@
 import { fetchApi } from '.';
 import IAttendanceCheck from '../Models/IAttendanceCheck';
+import ICourse from '../Models/ICourse';
 import IUser from '../Models/IUser';
 
 const _URLPREFIX = '/rolecall';
@@ -72,11 +73,11 @@ export const useBackendAttendance = () => {
 	/**
 	 * DELETE request to delete an attendance check by its ID.
 	 * @param attendanceCheckId the ID of the attendance check to delete
-	 * @returns string status in the form of "Course was deleted"
+	 * @returns string status in the form of "Attendance check was deleted"
 	 */
 	const deleteAttendanceCheck = async (attendanceCheckId: number) => {
 		return new Promise((resolve, reject) => {
-			fetchApi<string>(URL + `/delete/${attendanceCheckId}`, 'DELETE') //returns status in the form of string "Course was deleted"
+			fetchApi<string>(URL + `/delete/${attendanceCheckId}`, 'DELETE') //returns status in the form of string "Attendance check was deleted"
 				.then((response) => {
 					resolve(response);
 				})
@@ -107,16 +108,94 @@ export const useBackendAttendance = () => {
 	return { AddAttendance, getAllAttendanceChecks, getAttendanceCheck, deleteAttendanceCheck, addStudentToAttendanceCheck };
 };
 
+/**
+ * Requests to the backend Course service.
+ * @returns React hook of available services
+ */
 export const useBackendCourses = () => {
 	const URL = _URLPREFIX + '/courses';
-	const getApiOne = () => {};
-	const getApiTwo = () => {};
 
-	return { getApiOne, getApiTwo };
+	/**
+	 * POST request to add a new Course
+	 * @param dto The Course to add
+	 * @returns todo ?
+	 */
+	const addCourse = async (dto: ICourse) => {
+		return new Promise((resolve, reject) => {
+			fetchApi<string>(URL + '/add', 'POST', JSON.stringify(dto)) //todo unknown result, mongo.insertOne?
+				.then((response) => {
+					resolve(response);
+				})
+				.catch((error) => {
+					reject(error);
+				});
+		});
+	};
+
+	/**
+	 * GET request to get all courses
+	 * @returns an array of courses
+	 */
+	const getAllCourses = async () => {
+		return new Promise((resolve, reject) => {
+			fetchApi<ICourse[]>(URL + '/all')
+				.then((response) => {
+					resolve(response);
+				})
+				.catch((error) => {
+					reject(error);
+				});
+		});
+	};
+
+	/**
+	 * GET request to get a single course by its id
+	 * @param courseId
+	 * @returns The course if succesful
+	 */
+	const getCourse = async (courseId: number) => {
+		return new Promise((resolve, reject) => {
+			fetchApi<ICourse>(URL + `/${courseId}`)
+				.then((response) => {
+					resolve(response);
+				})
+				.catch((error) => {
+					reject(error);
+				});
+		});
+	};
+
+	/**
+	 * DELETE request to delete a course by its ID
+	 * @param courseId the ID of the course to delete
+	 * @returns string status in the form of "Course was deleted"
+	 */
+	const deleteCourse = async (courseId: number) => {
+		return new Promise((resolve, reject) => {
+			fetchApi<string>(URL + `/delete/${courseId}`, 'DELETE') //returns status in the form of string "Course was deleted"
+				.then((response) => {
+					resolve(response);
+				})
+				.catch((error) => {
+					reject(error);
+				});
+		});
+	};
+
+	return { addCourse, getAllCourses, getCourse, deleteCourse };
 };
 
+/**
+ * Requests to the backend User service
+ * @returns React hook with available services
+ */
 export const useBackendUsers = () => {
 	const URL = _URLPREFIX + '/users';
+
+	
+
+
+
 	const getApiOne = () => {};
 	const getApiTwo = () => {};
 
